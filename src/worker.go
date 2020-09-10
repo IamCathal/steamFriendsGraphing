44820cbe7ab2bc038a9e92dff11c129ab27d6708
IamCathal <cathalsocallaghan@gmail.com>
2020-09-11 00:53:35 +0100

    Add worker function

--- src/worker.go
+++ src/worker.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"sync"
+	"sync/atomic"
 )
 
 type jobsStruct struct {
 	level   int
-	steamID int
+	steamID string
+	// APIKey  string
 }
 
 type workerConfig struct {
@@ -40,6 +43,44 @@ func InitWorkerConfig(levelCap int) (*workerConfig, error) {
 	return workConfig, nil
 }
 
-func worker(jobs <-chan jobsStruct, results chan<- jobsStruct, workerConfig, activeJobs *int64) {
+func Worker(jobs <-chan jobsStruct, results chan<- jobsStruct, cfg workerConfig, activeJobs *int64) {
+	for {
+		cfg.jobsMutex.Lock()
+		job := <-jobs
+		cfg.jobsMutex.Unlock()
 
+		// Get friends list for this current user
+		friendsObj, err := GetFriends(job.steamID, os.Getenv("APIKEY"), cfg.wg)
+		CheckErr(err)
+
+		numFriends := len(friendsObj.FriendsList.Friends)
+
+		// For each friend we'll add them to the jobs queue if
+		// their level is within our range
+		for i := 0; i < numFriends; i++ {
+
+			indivFriends := jobsStruct{
+				level:   job.level + 1,
+				steamID: friendsObj.FriendsList.Friends[i].Steamid,
+			}
+
+			fmt.Printf("New friend [Level %d][From %s][SteamID %s][Len %d]\n", indivFriends.level, job.steamID, indivFriends.steamID, len(jobs))
+
+			// If their level is within range, we'll scrape them in the future
+			// and therefore we up the counter of activeJobs
+			if indivFriends.level <= cfg.levelCap {
+				atomic.AddInt64(activeJobs, 1)
+			}
+
+			cfg.resMutex.Lock()
+			results <- indivFriends
+			cfg.resMutex.Unlock()
+		}
+
+		cfg.activeJobsMutex.Lock()
+		atomic.AddInt64(activeJobs, -1)
+		cfg.activeJobsMutex.Unlock()
+
+		cfg.wg.Done()
+	}
 }
