fb88eefb792b9fc525c8aeacd91b2e8118dd2d01
IamCathal <cathalsocallaghan@gmail.com>
2020-09-23 00:49:30 +0100

    Add server test and util file

--- src/server/server.go
+++ src/server/server.go
@@ -12,34 +12,6 @@ import (
 	"github.com/steamFriendsGraphing/util"
 )
 
-type BasicResponse struct {
-	Status int    `json:"status"`
-	Body   string `json:"body"`
-}
-
-type config struct {
-	Level    string `json:"level"`
-	StatMode string `json:"statMode"`
-	TestKeys string `json:"testKeys"`
-	Workers  string `json:"workers"`
-	SteamID  string `json:"steamID"`
-}
-
-func DecodeBody(r *http.Request, vars map[string]string) (config, error) {
-	inputConfig := config{}
-	decoder := json.NewDecoder(r.Body)
-	err := decoder.Decode(&inputConfig)
-	if err != nil {
-		return inputConfig, err
-	}
-	vars["level"] = inputConfig.Level
-	vars["statMode"] = inputConfig.StatMode
-	vars["testKeys"] = inputConfig.TestKeys
-	vars["workers"] = inputConfig.Workers
-	vars["steamID"] = inputConfig.SteamID
-	return inputConfig, nil
-}
-
 func CrawlMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
@@ -60,32 +32,6 @@ func CrawlMiddleware(next http.Handler) http.Handler {
 
 		next.ServeHTTP(w, r)
 	})
-}
-
-// Log an API call to the console with it's details
-func LogCall(method, endpoint, status, startTimeString string, cached bool) {
-	statusColor := "\033[0m"
-	cacheString := ""
-
-	if cached {
-		cacheString = "[CACHE] "
-	}
-
-	startTime, err := strconv.ParseInt(startTimeString, 10, 64)
-	if err != nil {
-		startTime = -1
-	}
-	endTime := time.Now().UnixNano() / int64(time.Millisecond)
-	delay := endTime - startTime
-
-	// If the HTTP status given is 2XX, give it a nice
-	// green color, otherwise give it a red color
-	if status[0] == '2' {
-		statusColor = "\033[32m"
-	} else {
-		statusColor = "\033[31m"
-	}
-	fmt.Printf("[%s] %s%s %s %s%s%s %dms\n", time.Now().Format("02-Jan-2006 15:04:05"), cacheString, method, endpoint, statusColor, status, "\033[0m", delay)
 }
 
 // HomeHandler serves the content for the home page
--- src/server/util.go
+++ src/server/util.go
@@ -1,33 +1,64 @@
 package server
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
+)
 
-func Yuppa() {
-	fmt.Println("ok")
+type BasicResponse struct {
+	Status int    `json:"status"`
+	Body   string `json:"body"`
 }
 
-// // Log an API call to the console with it's details
-// func LogCall(method, endpoint, status, startTimeString string, cached bool) {
-// 	statusColor := "\033[0m"
-// 	cacheString := ""
+type Config struct {
+	Level    string `json:"level"`
+	StatMode string `json:"statMode"`
+	TestKeys string `json:"testKeys"`
+	Workers  string `json:"workers"`
+	SteamID  string `json:"steamID"`
+}
 
-// 	if cached {
-// 		cacheString = "[CACHE] "
-// 	}
+// LogCall logs a call to the console with it's details
+func LogCall(method, endpoint, status, startTimeString string, cached bool) {
+	statusColor := "\033[0m"
+	cacheString := ""
 
-// 	startTime, err := strconv.ParseInt(startTimeString, 10, 64)
-// 	if err != nil {
-// 		startTime = -1
-// 	}
-// 	endTime := time.Now().UnixNano() / int64(time.Millisecond)
-// 	delay := endTime - startTime
+	if cached {
+		cacheString = "[CACHE] "
+	}
 
-// 	// If the HTTP status given is 2XX, give it a nice
-// 	// green color, otherwise give it a red color
-// 	if status[0] == '2' {
-// 		statusColor = "\033[32m"
-// 	} else {
-// 		statusColor = "\033[31m"
-// 	}
-// 	fmt.Printf("[%s] %s%s %s %s%s%s %dms\n", time.Now().Format("02-Jan-2006 15:04:05"), cacheString, method, endpoint, statusColor, status, "\033[0m", delay)
-// }
+	startTime, err := strconv.ParseInt(startTimeString, 10, 64)
+	if err != nil {
+		startTime = -1
+	}
+	endTime := time.Now().UnixNano() / int64(time.Millisecond)
+	delay := endTime - startTime
+
+	// If the HTTP status given is 2XX, give it a nice
+	// green color, otherwise give it a red color
+	if status[0] == '2' {
+		statusColor = "\033[32m"
+	} else {
+		statusColor = "\033[31m"
+	}
+	fmt.Printf("[%s] %s%s %s %s%s%s %dms\n", time.Now().Format("02-Jan-2006 15:04:05"), cacheString, method, endpoint, statusColor, status, "\033[0m", delay)
+}
+
+// DecodeBody takes a typical request and assigns the configuration given
+func DecodeBody(r *http.Request, vars map[string]string) (Config, error) {
+	inputConfig := Config{}
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&inputConfig)
+	if err != nil {
+		return inputConfig, err
+	}
+	vars["level"] = inputConfig.Level
+	vars["statMode"] = inputConfig.StatMode
+	vars["testKeys"] = inputConfig.TestKeys
+	vars["workers"] = inputConfig.Workers
+	vars["steamID"] = inputConfig.SteamID
+	return inputConfig, nil
+}
