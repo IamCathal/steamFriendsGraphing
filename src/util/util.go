0734758477c657457e020bd36a60f641d500603f
IamCathal <cathalsocallaghan@gmail.com>
2021-03-30 20:54:50 +0100

    Util: Light refactoring of tests

--- src/util/util.go
+++ src/util/util.go
@@ -19,64 +19,29 @@ import (
 
 var (
 	Green = "\033[32m"
-	// Green color text for terminal printing
 	Red = "\033[31m"
-	// Red color text for terminal printing
 	White = "\033[0;37m"
-	// White color text for terminal printing
 )
 
-// GetUsername gets a username from a given steamID
+// GetUsername gets a username from a given steamID by querying the 
+// steam web API
 func GetUsername(apiKey, steamID string) (string, error) {
-
 	if valid := IsValidFormatSteamID(steamID); !valid {
 		return "", fmt.Errorf("invalid steamID format")
 	}
 
-	// Get the target username from the ID
-	targetURL := fmt.Sprintf("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=%s&steamids=%s",
-		apiKey, steamID)
-	res, err := http.Get(targetURL)
-	if err != nil {
-		return "", err
-	}
-	body, err := ioutil.ReadAll(res.Body)
-	defer res.Body.Close()
-	if err != nil {
-		return "", err
-	}
-
-	var userStatsObj UserStatsStruct
-	json.Unmarshal(body, &userStatsObj)
-
-	if len(userStatsObj.Response.Players) == 0 {
-		return "", fmt.Errorf("invalid steamID %s given", steamID)
-	}
-
-	return userStatsObj.Response.Players[0].Personaname, nil
+	userStatsObj, err := GetPlayerSummary(steamID, apiKey)
+	return userStatsObj.Response.Players[0].Personaname, err
 }
 
+// GetUserDetails gets profile details such as: steamID, username, time created
+// profile URL and avatar URL
 func GetUserDetails(apiKey, steamID string) (map[string]string, error) {
-	// Get the target username from the ID
-	targetURL := fmt.Sprintf("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=%s&steamids=%s",
-		apiKey, steamID)
-	res, err := http.Get(targetURL)
+	userStatsObj, err := GetPlayerSummary(steamID, apiKey)
 	if err != nil {
 		return nil, err
 	}
-	body, err := ioutil.ReadAll(res.Body)
-	defer res.Body.Close()
-	if err != nil {
-		return nil, err
-	}
-
-	var userStatsObj UserStatsStruct
-	json.Unmarshal(body, &userStatsObj)
-
-	if len(userStatsObj.Response.Players) == 0 {
-		return nil, fmt.Errorf("invalid steamID %s given", steamID)
-	}
-
+	
 	resMap := make(map[string]string)
 	resMap["SteamID"] = userStatsObj.Response.Players[0].Steamid
 	resMap["Username"] = userStatsObj.Response.Players[0].Personaname
@@ -84,27 +49,24 @@ func GetUserDetails(apiKey, steamID string) (map[string]string, error) {
 	resMap["ProfileURL"] = userStatsObj.Response.Players[0].Profileurl
 	resMap["AvatarURL"] = userStatsObj.Response.Players[0].Avatarfull
 
-	return resMap, nil
+	return resMap, err
 }
 
 // CreateUserDataFolder creates a folder for holding cache.
 // Can either be userData for regular use or testData when running under github actions.
 func CreateUserDataFolder() error {
-	// Create the cache folder to hold logs if it doesn't exist
-	cacheFolder := "userData"
+	cacheFolder := ""
 	if exists := IsEnvVarSet("testing"); exists {
-		cacheFolder = "testData"
+		cacheFolder = fmt.Sprintf("%s/testData", os.Getenv("BWD"))
+	} else {
+		cacheFolder = fmt.Sprintf("%s/userData", os.Getenv("BWD"))
 	}
 
-	_, err := os.Stat(fmt.Sprintf("%s", cacheFolder))
-	if os.IsNotExist(err) {
-		os.Mkdir("userData/", 0755)
-		return nil
+	if _, err := os.Stat(cacheFolder); os.IsNotExist(err) {
+		err = os.Mkdir(cacheFolder, 0755)
+		CheckErr(err)
 	}
-
-	if err != nil {
-		return err
-	}
+	
 	return nil
 }
 
@@ -113,19 +75,20 @@ func CheckErr(err error) {
 	if err != nil {
 		_, file, line, _ := runtime.Caller(1)
 		path, _ := os.Getwd()
-
 		log.Fatal(fmt.Sprintf(" %s - %s:%d", err, strings.TrimPrefix(file, path), line))
 	}
 }
 
-// IsValidFormatSteamID does a simple regex check to see if the
+// ThrowErr throws an error explicitly
+func ThrowErr(err error) {
+	CheckErr(err)
+}
+
+// IsValidFormatSteamID runs a simple regex check to see if the
 // steamID is in the valid format before calling the API
 func IsValidFormatSteamID(steamID string) bool {
 	match, _ := regexp.MatchString("([0-9]){17}", steamID)
-	if !match {
-		return false
-	}
-	return true
+	return match
 }
 
 // IsValidSteamID checks if a steamID is valid by calling the API
@@ -184,22 +147,24 @@ func IsEnvVarSet(envvar string) bool {
 // GetAPIKeys retrieves the API key(s) to make requests with
 // API keys must be stored in APIKEY(s).txt
 func GetAPIKeys() ([]string, error) {
-	// APIKEYS.txt MUST be in the root directory of the project
-	APIKeysLocation := "../APIKEYS.txt"
 	// Dirty fix for now. If testing then go test is invoked in the ./src
 	// directory and we should look in the parents parent directory for APIKEYS.txt
 	if exists := IsEnvVarSet("GITHUBACTIONS"); exists {
 		return []string{os.Getenv("APIKEY"), os.Getenv("APIKEY1")}, nil
 	}
 
+	// APIKEYS.txt MUST be in the root directory of the project
+	APIKeysLocation := fmt.Sprintf("%s../APIKEYS.txt", os.Getenv("BWD"))
+
 	file, err := os.Open(APIKeysLocation)
 	if err != nil {
-		// Check again in the parent's parent directory if invoked in /src
-		file, err = os.Open(fmt.Sprintf("../%s", APIKeysLocation))
-		if err != nil {
-			CheckErr(errors.New("No APIKEYS.txt file found"))
-		}
-
+		fmt.Println(fmt.Sprintf("%s../APIKEYS.txt", os.Getenv("BWD")))
+		CheckErr(errors.New("No APIKEYS.txt file found"))
+		// Check again in the parent's parent directory if invoked by a test
+		// file, err = os.Open(fmt.Sprintf("%s", APIKeysLocation))
+		// if err != nil {
+		// 	CheckErr(errors.New("No APIKEYS.txt file found"))
+		// }
 	}
 	defer file.Close()
 
@@ -226,11 +191,39 @@ func ExtractSteamIDs(args []string) ([]string, error) {
 	for _, arg := range args {
 		if valid := IsValidFormatSteamID(arg); valid {
 			validSteamIDs = append(validSteamIDs, arg)
-			fmt.Printf("%s is valid\n", arg)
 		}
 	}
 	if len(validSteamIDs) == 0 {
 		return validSteamIDs, fmt.Errorf("No valid steamIDs given")
 	}
+
 	return validSteamIDs, nil
+}
+
+// GetPlayerSummary gets a player summary through the steam web API
+func GetPlayerSummary(steamID, apiKey string) (UserStatsStruct, error) {
+	var userStatsObj UserStatsStruct
+	targetURL := fmt.Sprintf("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=%s&steamids=%s",
+		apiKey, steamID)
+	res, err := http.Get(targetURL)
+	CheckErr(err)
+
+	body, err := ioutil.ReadAll(res.Body)
+	defer res.Body.Close()
+	CheckErr(err)
+
+	json.Unmarshal(body, &userStatsObj)
+
+	if len(userStatsObj.Response.Players) == 0 {
+		err := fmt.Errorf("invalid steamID %s given", steamID)
+		return userStatsObj, err
+	}
+
+	return userStatsObj, nil
+}
+
+func SetBaseWorkingDirectory() {
+	path, err := os.Getwd()
+	CheckErr(err)
+	os.Setenv("BWD", path)
 }
--- src/util/util_test.go
+++ src/util/util_test.go
@@ -14,11 +14,17 @@ type testInput struct {
 
 func TestMain(m *testing.M) {
 	os.Setenv("testing", "")
+	path, err := os.Getwd()
+	CheckErr(err)
+	os.Setenv("BWD", fmt.Sprintf("%s/../", path))
+
+	fmt.Println(":: RemoveAll ../testData")
 	os.RemoveAll("../testData")
+	fmt.Println(":: Mkdir ../testData")
 	os.Mkdir("../testData", 0755)
 
 	code := m.Run()
-
+	fmt.Println(":: RemoveAll ../testData")
 	os.RemoveAll("../testData")
 	os.Exit(code)
 }
@@ -26,7 +32,7 @@ func TestMain(m *testing.M) {
 func getAPIKeysForTesting() []string {
 	apiKeys := make([]string, 0)
 
-	// When being test on the GitHub actions environment
+	// When being tested on the GitHub actions environment
 	// it should take keys from from the environment variables
 	// rather than the non existent APIKEYS.txt file
 	if exists := IsEnvVarSet("GITHUBACTIONS"); exists {
@@ -35,7 +41,6 @@ func getAPIKeysForTesting() []string {
 	} else {
 		apiKeySlice, err := GetAPIKeys()
 		CheckErr(err)
-
 		apiKeys = apiKeySlice
 	}
 
@@ -48,39 +53,31 @@ func TestAllAPIKeys(t *testing.T) {
 	CheckAPIKeys(apiKeys)
 }
 
+func TestValidGetUserDetails(t *testing.T) {
+	apiKeys := getAPIKeysForTesting()
+	steamID := "76561198076045001"
+
+	expectedDetails := make(map[string]string, 0)
+	expectedDetails["SteamID"] = steamID
+	expectedDetails["TimeCreated"] = "2012-11-18 06:49:56 +0000 GMT"
+
+	userDetails, _ := GetUserDetails(apiKeys[0], steamID)
+	if userDetails["SteamID"] != expectedDetails["SteamID"] {
+		t.Errorf("TestInvalidGetUserDetails: expected SteamID: %s but received SteamID: %s", expectedDetails["SteamID"], userDetails["SteamID"])
+	}
+
+	if userDetails["TimeCreated"] != expectedDetails["TimeCreated"] {
+		t.Errorf("TestInvalidGetUserDetails: expected TimeCreated: %s but received TimeCreated: %s", expectedDetails["TimeCreated"], userDetails["TimeCreated"])
+	}
+}
+
 func TestInvalidGetUserDetails(t *testing.T) {
 	apiKeys := getAPIKeysForTesting()
-	steamID := "bad input"
+	steamID := "eeee"
 
 	_, err := GetUserDetails(apiKeys[0], steamID)
 	if err == nil {
-		t.Error(err)
-	}
-}
-
-func TestGetUserDetails(t *testing.T) {
-	apiKeys := getAPIKeysForTesting()
-	steamID := "76561197960287930"
-
-	fmt.Println("")
-	fmt.Printf("==================== Print user details ====================\n")
-	resMap, err := GetUserDetails(apiKeys[0], steamID)
-	if err != nil {
-		t.Error(err)
-	}
-	for k, v := range resMap {
-		fmt.Printf("%13s: %s\n", k, v)
-	}
-	fmt.Printf("============================================================\n\n")
-}
-
-func TestInvalidUsername(t *testing.T) {
-	apiKeys := getAPIKeysForTesting()
-	steamID := "invalid username"
-
-	_, err := GetUsername(apiKeys[0], steamID)
-	if err == nil {
-		t.Errorf("can't get username for %s using key %s", steamID, apiKeys[0])
+		t.Errorf("TestInvalidGetUserDetails: expected error for steamID: %s\n", steamID)
 	}
 }
 
@@ -93,6 +90,17 @@ func TestGetUsername(t *testing.T) {
 		t.Errorf("can't get username for %s using key %s", apiKeys[0], steamID)
 	}
 }
+
+func TestInvalidGetUsername(t *testing.T) {
+	apiKeys := getAPIKeysForTesting()
+	steamID := "invalid username"
+
+	_, err := GetUsername(apiKeys[0], steamID)
+	if err == nil {
+		t.Errorf("can't get username for %s using key %s", steamID, apiKeys[0])
+	}
+}
+
 
 func TestCreateDataFolder(t *testing.T) {
 	err := CreateUserDataFolder()
@@ -139,4 +147,14 @@ func TestExtractSteamIDs(t *testing.T) {
 		t.Errorf("no error given for empty steamID slice")
 	}
 
+}
+
+func TestIsValidFormatSteamID(t *testing.T) {
+	if isValid := IsValidFormatSteamID("76561198087169600"); !isValid {
+		t.Errorf("TestIsValidFormatSteamID: expected %v for steamID: 76561198087169600\n", isValid)
+	}
+
+	if isValid := IsValidFormatSteamID("eeeeeeeee"); isValid {
+		t.Errorf("TestIsValidFormatSteamID: expected %v for steamID: eeeeeeeee\n", isValid)
+	}
 }
